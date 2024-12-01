a602b8e59c132bcb6c5526fef9ffe3b6a91901e4
nguyenconvapor <nguyenconvapor@gmail.com>
2024-12-01 12:32:29 +0700

    up

--- liteload.sh
+++ liteload.sh
@@ -5,5 +5,5 @@ script_dir="/root/lite"
 
 # Vòng lặp để chạy từng script
 for i in {1..12}; do
-    gnome-terminal --tab --title "Lite $i" -- bash -c "cd $script_dir; python3 dt$i.py; exec bash" &
+    gnome-terminal --tab --title "Lite $i" -- bash -c "cd $script_dir; python3 lite$i.py; exec bash" &
 done
